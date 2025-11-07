8676d8bbe21e6a7fa58598bd1285d713bfad8769
Shane Nye <shanen@sussexbeds.co.uk>
2025-11-07 11:14:43 +0000

    productData update api

--- public/js/productData.js
+++ public/js/productData.js
@@ -1333,7 +1333,7 @@ function applyUpdates() {
 
 
 
-const API_BASE = "http://localhost:10000"; // ✅ backend server
+const API_BASE = "https://suitepim.onrender.com"; // ✅ backend server
 
 async function pushUpdates() {
   const table = document.querySelector("table.csv-table");
